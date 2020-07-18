bfce735c1f52eef0a2fb70e9e10df4ce0185cbab
semeka13 <semeka13@yandex.ru>
2020-07-18 12:10:46 +1000

    ready for deploy

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "1165644015:AAFMXYgGRUbuEKCpHrdvPjlwIU5jSWIufWo"
+TOKEN = "1254102688:AAHx-ZbCjmMViUmsIzUD5K9CKyyhuplP6fU"
 places = {"владивосток": ["Светланская"], "москва": ["Красная площадь"], "санкт-питербург": ["Светланская"]}
