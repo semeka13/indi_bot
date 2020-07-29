5e04e335154b7c3377f15ad260ce8bc88b85c7a4
semeka13 <semeka13@yandex.ru>
2020-07-29 12:34:34 +1000

    ready for deploy

--- main.py
+++ main.py
@@ -40,14 +40,14 @@ def start(message):
     cid = message.from_user.id
     knownUsers.append(cid)
     userStep[cid] = 0
-    m = "Вас приветствует бот,которому вы можете оставить сообщение, если один из биоиндикаторов показал загрязнения в воздухе.\n\nВсе сообщения абсолютно анонимны.\nДля того,чтобы сообщить о проблеме отправьте /air_problem."
+    m = "Вас приветствует бот, которому Вы можете оставить сообщение, если один из биоиндикаторов показал загрязнения в воздухе.\n\nВсе сообщения абсолютно анонимны.\nДля того, чтобы сообщить о проблеме, отправьте /air_problem."
     bot.send_message(message.from_user.id, m)
 
 
 @bot.message_handler(commands=["air_problem"])
 def air_problem(message):
     userStep[message.from_user.id] = 1
-    bot.send_message(message.from_user.id, "Напишите в каком городе вы заметили проблему")
+    bot.send_message(message.from_user.id, "Напишите, в каком городе Вы заметили проблему.")
 
 
 @bot.message_handler(func=lambda message: get_user_step(message.from_user.id) == 1)
@@ -55,7 +55,7 @@ def stage_one(message):
     global city
     if message.text.lower() in config.places:
         city = message.text.lower()
-        m = "Выберите место, где вы заметили проблему:"
+        m = "Выберите место, где Вы заметили проблему:"
         keyboard = types.InlineKeyboardMarkup(True)
         for place in config.places[message.text.lower()]:
             keyboard.add(types.InlineKeyboardButton(place, callback_data=place))
@@ -70,13 +70,13 @@ def stage_one(message):
 def callback_buttons(call):
     global place
     userStep[call.from_user.id] = 2
-    bot.send_message(call.from_user.id, "А теперь опиши проблему, которую ты заметил и пришли её фото.")
+    bot.send_message(call.from_user.id, "А теперь опишите проблему, которую Вы заметили, и пришлите её фото.")
     place = call.data
 
 
 @bot.message_handler(func=lambda message: get_user_step(message.from_user.id) == 2)
 def stage_two(message):
-    bot.send_message(message.from_user.id, "Ваше сообщение принято и отправлено в Роскомнадзор, спасибо.")
+    bot.send_message(message.from_user.id, "Ваше сообщение принято, спасибо.")
     bot.send_message(787539657, f"Город: {city.capitalize()}\nМесто: {place}\nЖалоба: {message.text}")
 
 
